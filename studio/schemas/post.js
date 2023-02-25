7f2a3fe3b36dac66c8a8c810891cb7f175c70e12
COLLINS  TONUI <tckiprotich@gmail.com>
2023-02-25 10:47:47 +0000

    hello

--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -35,6 +35,7 @@ export default {
       type: "reference",
       to: { type: "author" }
     },
+    ,
     {
       name: "mainImage",
       title: "Main image",
@@ -82,21 +83,45 @@ export default {
     {
       name: "body",
       title: "Body",
-      type: "blockContent"
-    }
-  ],
-
-  preview: {
-    select: {
-      title: "title",
-      author: "author.name",
-      media: "mainImage"
-    },
-    prepare(selection) {
-      const { author } = selection;
-      return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`
-      });
-    }
-  }
-};
+      type: "array",
+      of: [
+        {
+          type: "block",
+          styles: [
+            { title: "Normal", value: "normal" },
+            { title: "H1", value: "h1" },
+            { title: "H2", value: "h2" },
+            { title: "H3", value: "h3" },
+            { title: "H4", value: "h4" },
+            { title: "Quote", value: "blockquote" }
+          ],
+          lists: [
+            { title: "Bullet", value: "bullet" },
+            { title: "Numbered", value: "number" }
+          ],
+          marks: {
+            decorators: [
+              { title: "Strong", value: "strong" },
+              { title: "Emphasis", value: "em" },
+              { title: "Code", value: "code" }
+            ],
+            annotations: [
+              {
+                name: "link",
+                type: "object",
+                title: "URL",
+                fields: [
+                  {
+                    name: "href",
+                    type: "url",
+                    title: "URL",
+                    validation: Rule =>
+                      Rule.uri({
+                        allowRelative: true,
+                        scheme: ["https", "http", "mailto", "tel"]
+                      })
+                  },
+                  {
+                    title: "Open in new tab",
+                    name: "blank",
+                    description: "Read
