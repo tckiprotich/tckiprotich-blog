84a1026953feaf8b82bd0e5f08903d964378a890
COLLINS  TONUI <tckiprotich@gmail.com>
2023-02-25 10:58:38 +0000

    hello

--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -87,41 +87,27 @@ export default {
       of: [
         {
           type: "block",
-          styles: [
-            { title: "Normal", value: "normal" },
-            { title: "H1", value: "h1" },
-            { title: "H2", value: "h2" },
-            { title: "H3", value: "h3" },
-            { title: "H4", value: "h4" },
-            { title: "Quote", value: "blockquote" }
-          ],
-          lists: [
-            { title: "Bullet", value: "bullet" },
-            { title: "Numbered", value: "number" }
-          ],
+          styles: [{ title: "Normal", value: "normal" }],
+          lists: [],
           marks: {
-            decorators: [
-              { title: "Strong", value: "strong" },
-              { title: "Emphasis", value: "em" },
-              { title: "Code", value: "code" }
-            ],
-            annotations: [
-              {
-                name: "link",
-                type: "object",
-                title: "URL",
-                fields: [
-                  {
-                    name: "href",
-                    type: "url",
-                    title: "URL",
-                    validation: Rule =>
-                      Rule.uri({
-                        allowRelative: true,
-                        scheme: ["https", "http", "mailto", "tel"]
-                      })
-                  },
-                  {
-                    title: "Open in new tab",
-                    name: "blank",
-                    description: "Read
+            decorators: [{ title: "Code", value: "code" }]
+          }
+        }
+      ]
+    }
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "mainImage"
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`
+      });
+    }
+  }
+};
