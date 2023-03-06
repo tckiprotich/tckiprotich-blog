c8d09e2785d7e493fa5a4836aa83e8e4f5d379eb
COLLINS  TONUI <tckiprotich@gmail.com>
2023-03-06 12:45:29 +0300

    Update Data.js

--- components/Data/Data.js
+++ components/Data/Data.js
@@ -2,6 +2,14 @@ const Data = [
     {
         id: 1,
         image_url: "https://cdn.dribbble.com/users/2919663/screenshots/18372585/media/22afc4757735732a96d5a9563202f729.png",
+        work_title: "Food Facts",
+        description: "This is a simple web app that allows you to scan the barcode of any food product and get its nutritional information instantly. ",
+        genre: "APIs",
+        link: "https://Food-facts.vercel.app/"
+    },
+    {
+        id: 2,
+        image_url: "https://cdn.dribbble.com/users/2919663/screenshots/18372585/media/22afc4757735732a96d5a9563202f729.png",
         work_title: "Maua",
         description: "Application for showcasing my mobile photography skills.Built Using Next Js , hygraphy cms, Tailwindcss ",
         genre: "Web Development",
