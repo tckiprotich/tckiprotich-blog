ce9661de6c5789f2bcd91734184ac8bcfa4838f1
COLLINS  TONUI <tckiprotich@gmail.com>
2023-03-06 12:52:40 +0300

    Update Data.js

--- components/Data/Data.js
+++ components/Data/Data.js
@@ -15,21 +15,14 @@ const Data = [
         genre: "Web Development",
         link: "https://maua.vercel.app/"
     },
-    {
-        id: 2,
-        image_url: "https://cdn.dribbble.com/users/2919663/screenshots/18372585/media/22afc4757735732a96d5a9563202f729.png",
-        work_title: "Home App",
-        description: "The Home app is a web application that lets you find your next home from the comfort of your browser.",
-        genre: "UX Case Study",
-        link: "https://www.uxfol.io/p/cb6f9f95/030a311a"
-    },
+    
     {
         id: 2,
         image_url: "https://cdn.dribbble.com/users/2919663/screenshots/18372585/media/22afc4757735732a96d5a9563202f729.png",
         work_title: "Portfolio",
-        description: "My portfolio website,the one you are currently on built using Next Js, Tailwind CSS and hosted on Vercel",
-        genre: "Web Development",
-        link: "https://collinsp.vercel.app/"
+        description: "My portfolio website,the one you are currently on built using Next Js, Tailwind CSS, sanity and hosted on Vercel",
+        genre: "Portfilio, Blog",
+        link: "https://tckiprotich.vercel.app/"
     }
     ,
 ];
